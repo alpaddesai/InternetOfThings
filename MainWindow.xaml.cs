f746f83abfca56677bb9d0d64d11922cd23223a5
unknown <alpa.gupta@gmail.com>
2019-07-03 09:23:42 -0400

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InternetOfThings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void IoTData_Click(object sender, RoutedEventArgs e)
+        {
+            IoTDataManagementAnalytics IoTDataManagementAnalyticsObject = new IoTDataManagementAnalytics();
+            IoTDataManagementAnalyticsObject.Show();
+        }
+
+        private void IoTApplications_Click(object sender, RoutedEventArgs e)
+        {
+            IoTApplications IoTApplicationsObject = new IoTApplications();
+            IoTApplicationsObject.Show();
+        }
+
+        private void Emerging_Internet_of_Things_Click(object sender, RoutedEventArgs e)
+        {
+            NewAdvances NewAdvancesObject = new NewAdvances();
+            NewAdvancesObject.Show();
+        }
+    }
+}
---
+++ NewAdvances.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InternetOfThings
+{
+    /// <summary>
+    /// Interaction logic for NewAdvances.xaml
+    /// </summary>
+    public partial class NewAdvances : Window
+    {
+        public NewAdvances()
+        {
+            InitializeComponent();
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void RichTextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void HealthCare_Click(object sender, RoutedEventArgs e)
+        {
+            HealthCareSystemMap HealthCareSystemMapObject = new HealthCareSystemMap();
+            HealthCareSystemMapObject.Show();
+        }
+    }
+}
